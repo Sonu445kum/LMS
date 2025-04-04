ee9ee661f2a2078ebb2ae4afbcdd6d728049f4fd
Sonu445Kum <sonuroy1619@gmail.com>
2025-04-04 20:35:29 +0530

    Add Course.Routes in the backend

--- server/Routes/Course.Route.js
+++ server/Routes/Course.Route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import isAuthenticated from "../Middlewares/isAuthenticated.js";
-import { createCourse } from "../Controllers/Course.Controller.js";
+import { createCourse, getAllAdminCourses } from "../Controllers/Course.Controller.js";
 const router = express.Router();
 //import controller functionality here
 
 //routes
 router.route("/create").post(isAuthenticated,createCourse);
-
+router.route("/").get(isAuthenticated,getAllAdminCourses);
 
 export default router;
